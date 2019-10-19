8281e88d3bd3d59ef4595c081fac7e2e9d7b3a4d
Gary Schwaeber <gschwaeb@gmail.com>
2019-10-19 23:40:26 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -9,12 +9,12 @@ def turn_count (board)
 end
 
 
-#  board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 
-#def current_player (board)
-#  if turn_count % 2 == 0
-  #  return "X"
-#  else
-#    return "O"
-#  end
-#end
+
+def current_player (board)
+    if turn_count % 2 == 0
+  return "X"
+  else
+    return "O"
+  end
+end
