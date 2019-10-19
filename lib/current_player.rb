9b2d2edd538dba683591b5810f073de9a64470da
Gary Schwaeber <gschwaeb@gmail.com>
2019-10-19 23:19:01 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -2,7 +2,7 @@ def turn_count (board)
     board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
     counter = 0
     turn_count.each do |boards|
-     boards == "X" || "O"
+    if boards == "X" || "O"
     counter += 1
   end
 #end
